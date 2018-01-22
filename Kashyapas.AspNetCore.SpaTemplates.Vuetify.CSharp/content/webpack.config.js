c64f8fec11dcac20abd07c2ffe64b981f31f3511
William Byrne <williamb@tvkit.com>
2018-01-22 17:47:58 -0500

    Native vue filenames (requires 'git log --follow' for history), IE support via babel, .gitignore for sln artifacts.

--- Kashyapas.AspNetCore.SpaTemplates.Vuetify.CSharp/content/webpack.config.js
+++ Kashyapas.AspNetCore.SpaTemplates.Vuetify.CSharp/content/webpack.config.js
@@ -11,10 +11,10 @@ module.exports = (env) => {
         stats: { modules: false },
         context: __dirname,
         resolve: { extensions: [ '.js', '.ts' ] },
-        entry: { 'main': './ClientApp/boot.ts' },
+        entry: {'main': ['whatwg-fetch', 'babel-polyfill', './ClientApp/boot.ts']},
         module: {
             rules: [
-                { test: /\.vue\.html$/, include: /ClientApp/, loader: 'vue-loader', options: { loaders: { js: 'awesome-typescript-loader?silent=true' } } },
+                { test: /\.vue$/, include: /ClientApp/, loader: 'vue-loader', options: { loaders: { js: 'awesome-typescript-loader?silent=true' } } },
                 { test: /\.ts$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
                 { test: /\.css$/, use: isDevBuild ? [ 'style-loader', 'css-loader' ] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
